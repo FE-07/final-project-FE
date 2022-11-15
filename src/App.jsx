4b5d3ed2ce126042f4187e4472b982e3991e7d42
zaq <85076901+zaq99@users.noreply.github.com>
2022-11-16 01:52:48 +0700

    tailwind install is done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
+import './index.css'
 
 function App() {
 
   return (
     <>
+      <h1 className="text-3xl font-bold underline">Tailwind berhasil terintall</h1>
     </>
   )
 }
