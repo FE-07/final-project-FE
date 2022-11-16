83a98b88601d0340b3c5d73437484ebb216da1c2
zaq <85076901+zaq99@users.noreply.github.com>
2022-11-16 22:02:53 +0700

    add modify app

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
+import Navbar from './component/Navbar'
 import './index.css'
 
 function App() {
 
   return (
     <>
+      <Navbar />
       <h1 className="text-3xl font-bold underline">Tailwind berhasil terintall</h1>
     </>
   )
