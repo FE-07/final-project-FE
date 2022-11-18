f349f6f64b075dcdf4ae9df49c7d190e0fac1012
dityaww <111202012838@mhs.dinus.ac.id>
2022-11-18 07:45:18 +0700

    Update on App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import Navbar from './component/Navbar'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import Navbar from "./component/Navbar";
 
-import Login from './pages/Login'
+import Login from "./pages/Login";
+import Home from "./pages/Home";
+import Register from "./pages/Register";
 
-import './index.css'
+import "./index.css";
 
 function App() {
-
   return (
     <>
-      {/* <Navbar /> */}
+      <Navbar />
+
+      {/* <Home /> */}
+
+      <Register />
+
       <Login />
+
       {/* <h1 className="text-3xl font-bold bg-sky-300 underline-offset-2">Hello</h1> */}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
