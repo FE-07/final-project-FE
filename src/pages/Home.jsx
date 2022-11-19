e36cc238aea45fa578fbeb8a78f7e4ea031a2afc
dityaww <111202012838@mhs.dinus.ac.id>
2022-11-19 21:10:27 +0700

    Add home-page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Button from "../component/Button";
 import homeImage from "../assets/home-img.png";
 
 function Home() {
@@ -13,9 +14,13 @@ function Home() {
                 For Youth, By Youth Together We Turn The World Into A{" "}
                 <span className="text-yellow-500">Brighter Place.</span>
               </p>
-              <button className="bg-sky-600 hover:bg-sky-700 py-2 lg:w-1/4 rounded-md text-white">
-                Login
-              </button>
+              <div className="lg:w-1/4">
+                <Button
+                  name="Login"
+                  bgcolor="bg-sky-600"
+                  hover="hover:bg-sky-700"
+                />
+              </div>
             </div>
           </div>
           <div className="basis-1/2">
