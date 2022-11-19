a08ed72beffe6ac821699418966d89cd65248289
dityaww <111202012838@mhs.dinus.ac.id>
2022-11-19 21:11:45 +0700

    Add register-page

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -9,18 +9,19 @@ import banner from "../assets/bannerDaftar.png";
 function Register() {
   return (
     <>
-      <div className="container mx-auto">
-        <img className="w-full" src={banner} alt="" />
+      <div className="container mx-auto lg:my-10 my-5 px-5">
+        <img className="w-full rounded-md" src={banner} alt="" />
+        <p className="lg:text-4xl text-2xl font-bold text-sky-600 lg:my-5 my-3">
+          Daftar
+        </p>
 
-        <div className="flex gap-5 justify-between">
+        <div className="flex gap-5 mb-3">
+          <Checkbox name={"Partner/Donatur"} />
+          <Checkbox name={"Penggalang Dana"} />
+        </div>
+
+        <div className="flex flex-col lg:flex-row gap-5 justify-between">
           <div className="basis-1/2">
-            <p className="text-4xl font-bold text-sky-600">Daftar</p>
-
-            <div className="flex gap-5 mb-3">
-              <Checkbox name={"Partner/Donatur"} />
-              <Checkbox name={"Penggalang Dana"} />
-            </div>
-
             <div className="flex flex-col gap-5">
               <InputText
                 name={"Username"}
@@ -45,12 +46,12 @@ function Register() {
             </div>
           </div>
 
-          <div className="basis-1/4">
+          <div className="lg:relative basis-1/4">
             <div className="flex flex-col gap-3 mb-5">
               <InputText
                 name={"Berkas 1"}
                 type={"text"}
-                placeholder={"masukkan berkas 1"}
+                placeholder={"masukkan data"}
               />
               <Button name="Upload Berkas" bgcolor="bg-sky-600" />
             </div>
@@ -58,9 +59,12 @@ function Register() {
               <InputText
                 name={"Berkas 1"}
                 type={"text"}
-                placeholder={"masukkan berkas 1"}
+                placeholder={"masukkan data"}
               />
               <Button name="Upload Berkas" bgcolor="bg-sky-600" />
+            </div>
+            <div className="lg:absolute lg:bottom-0 w-full my-5 lg:my-0">
+              <Button name="Register" bgcolor="bg-teal-600" />
             </div>
           </div>
         </div>
