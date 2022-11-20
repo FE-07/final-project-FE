bfd846b6593aaf81e157157e3d14da4783f9f4dc
dityaww <111202012838@mhs.dinus.ac.id>
2022-11-20 16:46:20 +0700

    update on app

--- src/App.jsx
+++ src/App.jsx
@@ -18,6 +18,7 @@ function App() {
   return (
     <>
       <Navbar />
+
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/galangdana" element={<GalangDana />} />
