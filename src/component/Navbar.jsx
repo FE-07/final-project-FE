d2cb069eb9141cbba0f84dac80e2e826abb9b380
zaq <85076901+zaq99@users.noreply.github.com>
2022-11-24 17:17:32 +0700

    update navbar

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,8 +1,11 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import { logoAyoBantu, IconUser } from "../assets";
 import "./../css/Navbar.css";
 
 const Navbar = () => {
+    const [login, isLogin] = useState(false)
+
     return (
         <>
             <header className="bg-white drop-shadow-xl">
@@ -12,7 +15,8 @@ const Navbar = () => {
                     </Link>
                     <div className="hidden md:flex items-center gap-8">
                         <Link to="/">Home</Link>
-                        <Link to="/galangdana">Galang Dana</Link>
+                        {!isLogin == true ? <Link to="/galangdana">Galang Dana</Link> : <Link to="/login">Galang Dana</Link>}
+
                         <Link
                             className="rounded-md px-4 text-white hover:bg-red-100 py-1 bg-sky-600"
                             to="/login"
